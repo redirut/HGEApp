a029446166f3f0e43cbbdb3ea93f3218a62bd46e
Alexander <redirut@gmail.com>
2020-09-04 02:08:16 +0300

    jenkins configure

--- src/main/java/com/example/hgeapp/HgeappApplication.java
+++ src/main/java/com/example/hgeapp/HgeappApplication.java
@@ -1,12 +1,19 @@
 package com.example.hgeapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class HgeappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HgeappApplication.class, args);
+        SpringApplicationBuilder applicationBuilder = new SpringApplicationBuilder(HgeappApplication.class)
+                .web(WebApplicationType.NONE);
+        applicationBuilder.build()
+                .addListeners(new ApplicationPidFileWriter("./pid.info"));
+        applicationBuilder.run(args);
     }
 }
