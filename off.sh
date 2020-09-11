3621e29ffdbf5f931bb035ee0f98e8cbfc2abce2
Alexander <redirut@gmail.com>
2020-09-11 20:35:27 +0300

    fix off script

--- off.sh
+++ off.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 FILE=/tmp/pid
+RESULT=/tmp/result
 if [ -f "$FILE" ]; then
-docker stop $(cat $FILE)
+  docker stop $(cat $FILE) > $RESULT
+  if [ "$(cat $RESULT)" = "$(cat $FILE)" ]; then
+    rm -rf $FILE
+  else
+    echo "Result failure"
+  fi
 else
-echo "File pid does not exist"
+  echo "File pid does not exist"
 fi
